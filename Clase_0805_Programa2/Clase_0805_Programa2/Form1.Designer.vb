23e2cfb4ced72db92fff5069d5e155b1a5f9be07
SebasVGC <2902340180301@ingenieria.usac.edu.gt>
2022-08-05 16:21:35 -0600

    ejemplo 2clase 3

--- Clase_0805_Programa2/Clase_0805_Programa2/Form1.Designer.vb
+++ Clase_0805_Programa2/Clase_0805_Programa2/Form1.Designer.vb
@@ -26,17 +26,18 @@ Partial Class Form1
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.RadioButton3 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(318, 66)
+        Me.Button1.Location = New System.Drawing.Point(328, 66)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -45,100 +46,120 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(183, 66)
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(62, 66)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(100, 31)
         Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "0"
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(54, 66)
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(194, 66)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(100, 31)
         Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "0"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(94, 32)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(69, 32)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.Size = New System.Drawing.Size(68, 25)
         Me.Label1.TabIndex = 3
-        Me.Label1.Text = "A"
+        Me.Label1.Text = "Altura"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(203, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 25)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Peso"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(294, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(109, 25)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Resultado"
         '
         'RadioButton1
         '
         Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(117, 158)
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(28, 141)
         Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(52, 17)
-        Me.RadioButton1.TabIndex = 4
+        Me.RadioButton1.Size = New System.Drawing.Size(59, 24)
+        Me.RadioButton1.TabIndex = 10
         Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "Suma"
+        Me.RadioButton1.Text = "7-20"
         Me.RadioButton1.UseVisualStyleBackColor = True
         '
         'RadioButton2
         '
         Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(117, 194)
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(28, 171)
         Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(53, 17)
-        Me.RadioButton2.TabIndex = 5
+        Me.RadioButton2.Size = New System.Drawing.Size(68, 24)
+        Me.RadioButton2.TabIndex = 11
         Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "Resta"
+        Me.RadioButton2.Text = "21-50"
         Me.RadioButton2.UseVisualStyleBackColor = True
         '
         'RadioButton3
         '
         Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(117, 232)
+        Me.RadioButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.Location = New System.Drawing.Point(28, 210)
         Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(89, 17)
-        Me.RadioButton3.TabIndex = 6
+        Me.RadioButton3.Size = New System.Drawing.Size(54, 24)
+        Me.RadioButton3.TabIndex = 12
         Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "Multiplicación"
+        Me.RadioButton3.Text = ">50"
         Me.RadioButton3.UseVisualStyleBackColor = True
         '
-        'RadioButton4
+        'Label4
         '
-        Me.RadioButton4.AutoSize = True
-        Me.RadioButton4.Location = New System.Drawing.Point(117, 271)
-        Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(62, 17)
-        Me.RadioButton4.TabIndex = 7
-        Me.RadioButton4.TabStop = True
-        Me.RadioButton4.Text = "División"
-        Me.RadioButton4.UseVisualStyleBackColor = True
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(25, 118)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 20)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Edad"
         '
-        'Label2
+        'Label5
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(227, 32)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(14, 13)
-        Me.Label2.TabIndex = 8
-        Me.Label2.Text = "B"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(301, 207)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(55, 13)
-        Me.Label3.TabIndex = 9
-        Me.Label3.Text = "Resultado"
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(295, 191)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(122, 20)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Resultado edad"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(482, 360)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.RadioButton4)
+        Me.ClientSize = New System.Drawing.Size(436, 246)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.RadioButton3)
         Me.Controls.Add(Me.RadioButton2)
         Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
@@ -154,10 +175,11 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
     Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents RadioButton2 As RadioButton
     Friend WithEvents RadioButton3 As RadioButton
-    Friend WithEvents RadioButton4 As RadioButton
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 End Class
--- Clase_0805_Programa2/Clase_0805_Programa2/Form1.vb
+++ Clase_0805_Programa2/Clase_0805_Programa2/Form1.vb
@@ -1,3 +1,49 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim altura As Double = CDbl(TextBox1.Text)
+        Dim peso As Double = CDbl(TextBox2.Text)
+        Dim resultado As String
 
+        If (altura < 1.5) Then
+            If (peso < 40) Then
+                resultado = "Delgado"
+            ElseIf (peso >= 40 And peso <= 55) Then
+                resultado = "normal"
+            ElseIf (peso > 55) Then
+                resultado = "Obeso"
+            End If
+        ElseIf (altura >= 1.5 And altura <= 1.7) Then
+            If (peso < 45) Then
+                resultado = "Delgado"
+            ElseIf (peso >= 45 And peso <= 65) Then
+                resultado = "Normal"
+            ElseIf (peso > 65) Then
+                resultado = "Obeso"
+            End If
+        ElseIf (altura > 1.7) Then
+            If (peso < 60) Then
+                resultado = "Delgado"
+            ElseIf (peso >= 60 And peso <= 80) Then
+                resultado = "Normal"
+            ElseIf (peso > 80) Then
+                resultado = "Obeso"
+            End If
+        End If
+
+        'Imprimir el resultado
+        Label3.Text = resultado
+
+        'Saber cual radioButton esta seleccionado
+        Dim resultadoEdad As String
+        If (RadioButton1.Checked) Then
+            resultadoEdad = "Joven"
+        ElseIf (RadioButton2.Checked) Then
+            resultadoEdad = "Adulto"
+        ElseIf (RadioButton3.Checked) Then
+            resultadoEdad = "Anciano"
+        End If
+
+        Label5.Text = resultadoEdad
+
+    End Sub
 End Class
