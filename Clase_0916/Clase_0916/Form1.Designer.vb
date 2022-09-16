af25d762bbf15d3a18b15b27b98df17ee90c995d
SebasVGC <83033498+SebasVGC@users.noreply.github.com>
2022-09-16 16:53:13 -0600

    Clase 16 septiembre

---
+++ Clase_0916/Clase_0916/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(270, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(207, 53)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Conexion"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(34, 164)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(207, 56)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Ver todos los datos"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(34, 245)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(740, 150)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(543, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(197, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Estado: Sin conexion"
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(567, 164)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(207, 56)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Ver edad > 14"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(342, 181)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Clase_0916/Clase_0916/Form1.vb
@@ -0,0 +1,125 @@
+﻿Imports System.Data.SqlClient
+Imports System.Net
+
+Public Class Form1
+    Dim datosConexion As String = "Data Source=DESKTOP-HH0VCVP\SQLEXPRESS;Initial Catalog=ejemplo1;Integrated Security=True"
+    Dim conexion As New SqlConnection(datosConexion)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            conexion.Open()
+            MsgBox("Conexion con la BD exitosa")
+            Label1.Text = "Estado: Conectado"
+            Button2.Enabled = True
+            Button3.Enabled = True
+
+            conexion.Close()
+        Catch ex As Exception
+            MsgBox("Error al conectar la DB")
+        End Try
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim MiCmd As SqlCommand
+        Dim adaptador As SqlDataAdapter
+        Dim setDatos As DataSet
+
+        Dim consulta1 As String = "
+            SELECT * FROM alumnos
+            ;
+        "
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Rows.Clear()
+        DataGridView1.Columns.Clear()
+
+
+        conexion.Open()
+        Try
+            MiCmd = conexion.CreateCommand
+            Dim consulta As String = consulta1
+            MiCmd.CommandText = consulta
+            MiCmd.CommandType = CommandType.Text
+            MiCmd.Connection = conexion
+            adaptador = New SqlDataAdapter(MiCmd)
+            setDatos = New DataSet()
+            adaptador.Fill(setDatos)
+            DataGridView1.DataSource = setDatos.Tables(0).DefaultView
+
+        Catch ex As Exception
+            MsgBox("Error al realizar consulta")
+        End Try
+        conexion.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim MiCmd As SqlCommand
+        Dim adaptador As SqlDataAdapter
+        Dim setDatos As DataSet
+
+        Dim consulta1 As String = "
+            SELECT * FROM alumnos
+            WHERE edad > 14
+            ;
+        "
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Rows.Clear()
+        DataGridView1.Columns.Clear()
+
+
+        conexion.Open()
+        Try
+            MiCmd = conexion.CreateCommand
+            Dim consulta As String = consulta1
+            MiCmd.CommandText = consulta
+            MiCmd.CommandType = CommandType.Text
+            MiCmd.Connection = conexion
+            adaptador = New SqlDataAdapter(MiCmd)
+            setDatos = New DataSet()
+            adaptador.Fill(setDatos)
+            DataGridView1.DataSource = setDatos.Tables(0).DefaultView
+
+        Catch ex As Exception
+            MsgBox("Error al realizar consulta")
+        End Try
+        conexion.Close()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim MiCmd As SqlCommand
+        Dim MiLector As SqlDataReader
+        Try
+            MiCmd = conexion.CreateCommand
+            Dim consulta As String = "SELECT * FROM alumnos WHERE nombre='Luis'"
+            MiCmd.CommandText = consulta
+
+            conexion.Open()
+            MiLector = MiCmd.ExecuteReader()
+
+            Dim carnet As String = ""
+            Dim nombre As String = ""
+
+
+
+            Do While MiLector.Read()
+                '0-carnet;  1= nombre...
+                carnet = MiLector.GetInt32(0).ToString()
+                nombre = MiLector.GetString(1)
+
+            Loop
+
+            'If (nombre = textbox2.text) Then
+            '    MsgBox("Se llama luis")
+            'End If
+
+            conexion.Close()
+
+        Catch ex As Exception
+            MsgBox("Error al buscar usuario")
+        End Try
+    End Sub
+End Class
